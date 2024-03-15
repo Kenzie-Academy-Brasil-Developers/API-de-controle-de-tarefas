fdd6aa0ec274b03650118f6f0cc424e375e51e22
Fabio Henrique <110111015+maktubz1@users.noreply.github.com>
2024-03-15 17:14:25 -0300

    Add files via upload

---
+++ taskService.ts
@@ -0,0 +1,55 @@
+import { category } from "./../tests/mocks/category.mocks";
+import { prisma } from "../database/prisma";
+import { taskReturnSchema, taskSchema } from "../schemas/task.schema";
+import {
+  TaskCreate,
+  TaskReturnCategory,
+  TaskSchema,
+  TaskUpdate,
+} from "../interface/task.interface";
+
+export class TaskService {
+  //  criar uma nova tarefa
+  create = async (body: TaskCreate, id: number): Promise<TaskSchema> => {
+    return taskSchema.parse(await prisma.task.create({ data: body }));
+  };
+
+  //  obter todas as tarefas
+  findMany = async (
+    categoryName?: string
+  ): Promise<Array<TaskReturnCategory>> => {
+    const tasks = await prisma.task.findMany({
+      where: {
+        ...(categoryName && { category: { name: categoryName } }),
+      },
+      include: { category: true },
+    });
+
+    return tasks;
+  };
+
+  //  atualizar uma tarefa existente
+  update = async (id: number, payload: TaskUpdate): Promise<TaskSchema> => {
+    const update = await prisma.task.update({
+      where: { id: Number(id) },
+      data: payload,
+    });
+    return taskReturnSchema.parse(update);
+  };
+
+  //  obter uma única tarefa pelo ID
+  findOne = async (id: number): Promise<TaskSchema | null> => {
+    const singleTask = await prisma.task.findUnique({
+      where: {
+        id,
+      },
+      include: { category: true },
+    });
+    return singleTask;
+  };
+
+  //  excluir uma tarefa existente
+  delete = async (id: number) => {
+    await prisma.task.delete({ where: { id } });
+  };
+}
